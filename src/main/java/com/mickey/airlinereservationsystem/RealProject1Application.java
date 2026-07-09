c44db22f8461f84461a67422febf05b3e6fc5a98
Mickey <bhaskarp986@gmail.com>
2026-07-09 11:12:17 +0530

    feat: initial commit feat: created a new project to house the Airline reservation system

---
+++ src/main/java/com/mickey/airlinereservationsystem/RealProject1Application.java
@@ -0,0 +1,13 @@
+package com.mickey.airlinereservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealProject1Application.class, args);
+    }
+
+}
