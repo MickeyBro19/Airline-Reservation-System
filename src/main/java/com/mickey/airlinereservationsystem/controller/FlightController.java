ce6d37ca21157f0af2b6da40c437e38eea0baf29
Mickey <bhaskarp986@gmail.com>
2026-07-11 12:08:23 +0530

    feat: implement flight management with airport validation and pagination

---
+++ src/main/java/com/mickey/airlinereservationsystem/controller/FlightController.java
@@ -0,0 +1,41 @@
+package com.mickey.airlinereservationsystem.controller;
+import com.mickey.airlinereservationsystem.dto.FlightRequest;
+import com.mickey.airlinereservationsystem.dto.FlightResponse;
+import com.mickey.airlinereservationsystem.service.interfaces.FlightService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/flight")
+@RequiredArgsConstructor
+public class FlightController {
+    private final FlightService flightService;
+
+
+    @GetMapping
+    public Page<FlightResponse> getAllFlights(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "id") String sortBy){
+        return flightService.getAllFlights(
+                page,size,sortBy
+        );
+    }
+
+    @GetMapping("/{flightNumber}")
+    public FlightResponse getFlightByFlightNumber(@PathVariable String flightNumber){
+        return flightService.getFlight(flightNumber);
+    }
+
+    @PostMapping
+    public FlightResponse createFlight(@Valid @RequestBody FlightRequest request){
+        return flightService.createFlight(request);
+    }
+
+    @DeleteMapping("/{flightNumber}")
+    public void deleteFlight(@PathVariable String flightNumber){
+        flightService.deleteFlight(flightNumber);
+    }
+}
